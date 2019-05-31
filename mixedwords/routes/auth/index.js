e0d2522984e5de61c3e67e3eb349fde523dcf64b
Belkheiri Amine <30414257+RainbowSoul@users.noreply.github.com>
2019-05-31 18:25:08 +0200

    Ajout d'un background animé, premier design de la page profil et récupération du nom d'utilisateur sur le layout plus un bouton éditer

--- mixedwords/routes/auth/index.js
+++ mixedwords/routes/auth/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var UserController = require('../../controllers/UserController.js');
-var router = express.Router();
-var app = express();
+const express = require('express');
+const UserController = require('../../controllers/UserController.js');
+const router = express.Router();
 
-// Requêtes GET sur la page d'accueil
+// Sert la page d'accueil si on la demande via une requête GET.
 router.get('/', function(req, res) {
     res.render('home');
 });
@@ -11,38 +10,40 @@ router.get('/home', function(req, res) {
     res.render('home');
 });
 
-// Requête GET sur la page de jeu ( Accessible seulement si connecté )
+// Sert la page des paramètres de jeu si on la demande via une requête GET.
+// Accessible seulement si on est connecté.
 router.get('/gamesettings', isLoggedIn, function(req, res) {
     res.render('gamesettings');
 });
 
-//requête GET sur la page de connexion
+// Sert la page de connexion si on la demande via une requête GET.
 router.get('/connection', function(req, res) {
-    if(!req.user) {
-        res.render('connection', { userLogged: false });
-    }
+    res.status(200).render('connection');
 });
 
-
+// Sert la page d'inscription si on la demande via une requête GET.
 router.get('/register', function(req, res) {
-    res.render('register');
+    res.status(200).render('register');
 });
 
-
+// Sert la page de profile si on la demande via une requête GET.
+// Accessible seulement si on est connecté.
 router.get('/profile', isLoggedIn, (req, res) => {
-    res.status(200).json(req.user);
+    res.status(200).render('profile');
 });
 
-
+// Permet de clôturer la session utilisateur en cours lorsque que l'on clique sur l'icône de déconnexion.
 router.get('/logout', isLoggedIn, (req, res) => {
     req.logout();
     res.redirect('home');
 });
 
+// Permet de récupérer les informations envoyées par l'utilisateur lors de la soumission d'un formulaire via une requête POST.
 router.post('/register', UserController.create);
 
 module.exports = router;
 
+// Middleware permettant de vérifier si un utilisateur est log ( connecté ) ou pas.
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
